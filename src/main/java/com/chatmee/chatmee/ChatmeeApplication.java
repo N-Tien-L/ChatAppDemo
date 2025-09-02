9c1b3ace73d0dfa4a932339d9fb96a478831b7ba
Tien Lam <tienlamnhatt@gmail.com>
2025-09-02 20:19:50 +0700

    initial commit

---
+++ src/main/java/com/chatmee/chatmee/ChatmeeApplication.java
@@ -0,0 +1,13 @@
+package com.chatmee.chatmee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatmeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatmeeApplication.class, args);
+	}
+
+}
